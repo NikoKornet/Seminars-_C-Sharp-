b9aa93ddde804d813fe1f7ddc7edcdde1c824801
Nikolay <nikornetov@mail.ru>
2022-09-20 22:54:43 +0300

    Break

--- Task01/Program.cs
+++ Task01/Program.cs
@@ -3,3 +3,16 @@
 // a = 2, b = 10 -> нет
 // a = 9, b = -3 -> да
 // a = -3, b = 9 -> нет
+
+Console.Write("Введите первое число ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Ведите второе число ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+if(number1 == number2 * number2)
+{
+    Console.Write("Первое число является квадратом второго");
+}
+else 
+{
+    Console.Write("Первое число не является квадратом второго");
+}
