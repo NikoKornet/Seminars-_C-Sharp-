37253fcec4a3cc7fd2b65925abce82b0cb0f0fde
Nikolay <nikornetov@mail.ru>
2022-09-28 11:13:32 +0300

    Add HomeWork_Task10

---
+++ HomeWork_Task10/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+Console.WriteLine("Введите число от 100 до 1000: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int SecondDigit(int num)
+{
+    int thirdDigit = num / 10;
+    int secondDigit = thirdDigit % 10;
+    return secondDigit;
+}
+
+int result = SecondDigit(number);
+Console.WriteLine($"Вторая цифра числа {number} => {result}");
+ 
