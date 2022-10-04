c381828cab9e0dd1922e94c9948223d324214ebe
Nikolay <nikornetov@mail.ru>
2022-10-04 19:33:18 +0300

    Add HomeWork Task19

--- HomeWork_Task19/Program.cs
+++ HomeWork_Task19/Program.cs
@@ -3,3 +3,22 @@
 // 14212 -> нет
 // 12821 -> да
 // 23432 -> да
+
+Console.Write("Введите пятизначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int reversNumber = reverseDigits(number);
+
+if (reversNumber == number) Console.WriteLine($"{number} - является палиндромом!");
+else Console.WriteLine($"{number} - не является полиндромом!");
+
+int reverseDigits(int num)
+{
+    int reversNum = default;
+    while (num > 0)
+    {
+        reversNum = reversNum * 10 + num % 10;
+        num = num / 10;
+    }
+    return reversNum;
+}
