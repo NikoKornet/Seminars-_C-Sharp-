f8221d97bfa0ee288b582489ef61a69a4542d583
Nikolay <nikornetov@mail.ru>
2022-10-09 21:48:25 +0300

    HomeWork Task36

---
+++ Task32/Program.cs
@@ -0,0 +1,40 @@
+﻿// Задача 32: Напишите программу замена элементов
+// массива: положительные элементы замените на
+// соответствующие отрицательные, и наоборот.
+// [-4, -8, 8, 2] -> [4, 8, -8, -2]
+
+int[] arr = CreateArrayRndInt(10, -9, 9);
+PrintArray(arr);
+InversionNum(arr);
+PrintArray(arr);
+
+int[] CreateArrayRndInt(int size, int min, int max)
+{
+    int[] array = new int[size];
+    var rnd = new Random();
+
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = rnd.Next(min, max + 1);
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i < array.Length - 1) Console.Write($"{array[i]}, ");
+        else Console.Write($"{array[i]}");
+    }
+    Console.WriteLine("]");
+}
+
+void InversionNum(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = -array[i];
+    }
+}
---
+++ Task33/Program.cs
@@ -0,0 +1,51 @@
+﻿// Задача 33: Задайте массив. Напишите программу, которая
+// определяет, присутствует ли заданное число в массиве.
+// 4; массив [6, 7, 19, 345, 3] -> нет
+// 3; массив [6, 7, 19, 345, 3] -> да
+
+Console.Write("Введите искомое число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int[] arr = CreateArrayRndInt(10, 1, 1000);
+PrintArray(arr);
+bool checkNumber = CheckNumber(arr, num);
+if (checkNumber) Console.WriteLine($"{num} - число присутствует в массиве");
+else Console.WriteLine($"{num} - отсутствует в массиве");
+
+
+int[] CreateArrayRndInt(int size, int min, int max)
+{
+    int[] array = new int[size];
+    var rnd = new Random();
+
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = rnd.Next(min, max);
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i < array.Length - 1) Console.Write($"{array[i]}, ");
+        else Console.Write($"{array[i]}");
+    }
+    Console.WriteLine("]");
+}
+
+bool CheckNumber(int[] array, int arg)
+{
+    bool checkNumber = false;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] == arg)
+        {
+            checkNumber = true;
+            break;
+        }
+    }
+    return checkNumber;
+}    
