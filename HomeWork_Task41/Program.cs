c021243839edb360295f6dc78fbe96aa27f4de8d
Nikolay <nikornetov@mail.ru>
2022-10-12 12:26:38 +0300

    Add Task67 & Task69

--- HomeWork_Task41/Program.cs
+++ HomeWork_Task41/Program.cs
@@ -1,5 +1,24 @@
 ﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
 
 // 0, 7, 8, -2, -2 -> 2
-
 // -1, -7, 567, 89, 223-> 3
+
+Console.Write("Введите колличество задаваемых чисел M = ");
+int numberM = Convert.ToInt32(Console.ReadLine());
+
+int countElemGreaterThanZero = CountElemGreaterThanZero(numberM);
+
+if(countElemGreaterThanZero == 0) Console.Write("Введено число несоответствующее условию");
+else Console.Write($"Колличество чисел больше 0 -> {countElemGreaterThanZero}");
+
+int CountElemGreaterThanZero(int size)
+{
+    int counter = 0;
+    for (int i = 0; i < size; i++)
+    {
+        Console.Write("Введите число: ");
+        int num = Convert.ToInt32(Console.ReadLine());
+        if (num > 0) counter++;
+    }
+    return counter;
+}
---
+++ Task67/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 67: Напишите программу, которая будет принимать на вход число и
+// возвращать сумму его цифр.
+// 453 -> 12
+// 45 -> 9
+
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int sum = DigitSumInNumber(number);
+Console.WriteLine(sum);
+
+int DigitSumInNumber(int num)  
+{
+    int sumOfDigits = num % 10;      
+    if (num > 0) sumOfDigits += DigitSumInNumber(num / 10); 
+    return sumOfDigits;
+}
